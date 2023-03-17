feff89e14f2b196b9b26f59520e060d8f11ce2dc
ThotakuraHarish2003 <127377804+HarishTQ@users.noreply.github.com>
2023-03-17 15:22:04 +0530

    Completed Ex2.5

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map(part => <Part part={part} />)}
+    </>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={course.parts.reduce((s,p)=>s+p.exercises,0)} />
+    </>
+  )
+}
+
+export default Course
