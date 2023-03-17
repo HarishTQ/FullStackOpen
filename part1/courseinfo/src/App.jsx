3a2928e68b0859d0111429947904901442197903
ThotakuraHarish2003 <127377804+HarishTQ@users.noreply.github.com>
2023-03-17 10:07:37 +0530

    Completed Ex1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const Header = ({courseName}) => {
+const Header = ({ courseName }) => {
   return <h1>{courseName}</h1>
 }
 
-const Part = ({data}) => {
+const Part = ({ data }) => {
   return <p>{data.name} {data.exercises}</p>
 }
 
-const Content = ({parts}) => {
+const Content = ({ parts }) => {
   return (
     <>
       <Part data={parts[0]} />
@@ -18,30 +18,32 @@ const Content = ({parts}) => {
   )
 }
 
-const Total = ({parts}) => {
-  return <p>Number of exercises {parts[0].exercises+parts[1].exercises+parts[2].exercises}</p>
+const Total = ({ parts }) => {
+  return <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
 }
 
-  const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = {
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
       name: 'Fundamentals of React',
       exercises: 10
-    }
-    const part2 = {
+    },
+    {
       name: 'Using props to pass data',
       exercises: 7
-    }
-    const part3 = {
+    },
+    {
       name: 'State of a component',
       exercises: 14
     }
+  ]
 
   return (
     <div>
-      <Header courseName={course}/>
-      <Content parts={[part1,part2,part3]}/>
-      <Total parts={[part1,part2,part3]}/>
+      <Header courseName={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
