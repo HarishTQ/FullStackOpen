83c39e2bc86b363da75e4c74d74baf9771bbbf89
HarishTQ <127377804+HarishTQ@users.noreply.github.com>
2023-03-19 14:30:58 +0530

    Completed upto 3.18

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -27,6 +27,17 @@ let Phonebook = [
         "number": "39-23-6423122"
     }
 ]
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+  }
+
+const unknownEndpoint = (request, response) => {
+response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.use(express.json())
 app.use(morgan((tokens,req,res)=>{
@@ -47,13 +58,14 @@ app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has infor for ${Phonebook.length} people</p><p>${new Date()}</p>`)
 })
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res,next) => {
     Contact.find({}).then((result)=>{
         res.json(result)
     })
+    .catch(error=>next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res,next) => {
     const body = req.body;
     if (!body.name || !body.number) {
         return res.status(400).json({ error: 'name or number absent' })
@@ -68,26 +80,40 @@ app.post('/api/persons', (req, res) => {
     newContact.save().then((result)=>{
         res.json(result).status(202)
     })
+    .catch(error=>next(error))
     
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = req.params.id;
-    const person = Phonebook.find(person => person.id == id)
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+app.put('/api/persons/:id',(req,res,next)=>{
+    const body = req.body;
+    const contact = {name:body.name,number:body.number}
+
+    Contact.findByIdAndUpdate(req.params.id,contact,{new:true})
+            .then((response)=>{res.json(response)})
+            .catch(error=>next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res,nexy) => {
     const id = req.params.id;
-    Phonebook = Phonebook.filter(data => data.id != id)
-    res.status(204).end()
+    Contact.findById(req.params.id)
+        .then((result)=>{
+            res.json(result)
+        })
+        .catch(error=>next(error))
 })
 
+app.delete('/api/persons/:id', (req, res,next) => {
+    const id = req.params.id;
+    Contact.findByIdAndRemove(id)
+        .then((result)=>{
+            res.status(204).end()
+        })
+        .catch(error=>next(error))
+})
 
+app.use(errorHandler)
+app.use(unknownEndpoint)
+  
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
